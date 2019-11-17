1d3890260289fcdc924514fd4ab42e1396510e82
Acxel Morales <acxelmorales97@gmail.com>
2019-11-16 18:46:56 -0600

    Quotes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import Frase from './components/Frase';
 
 function App() {
+  const [frase, setFrase] = useState({});
+
+  const consultarAPI = async () => {
+    const resultado = await axios.get('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    setFrase(resultado.data[0]);
+  };
+
+  useEffect(() => {
+    consultarAPI();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor">
+      <Frase
+        frase={frase}
+      />
+      <button
+        onClick={consultarAPI}
+      >
+        Generar Nueva
+      </button>
     </div>
   );
 }
